54448e8445af22fcc043cc29298612e3835435af
ruskava <94966888+ruskava@users.noreply.github.com>
2022-08-21 16:58:05 +0300

    доработка task7

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,5 @@
 const inpEl = document.querySelector('#font-size-control');
-const spanEl = document.querySelector('text');
+const spanEl = document.querySelector('#text');
 
 inpEl.addEventListener('mousemove', () => {
     spanEl.style.fontSize = `${inpEl.value}px`;
