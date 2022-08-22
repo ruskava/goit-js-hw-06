c10a27067e3ca8f3c814e6c969bece200852f878
ruskava <94966888+ruskava@users.noreply.github.com>
2022-08-22 17:51:25 +0300

    task6,8

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,16 @@
-const input = document.querySelector('#validation-input');
-input.addEventListener('blur', onBlur);
+const input = document.querySelector("#validation-input");
+input.addEventListener("blur", onBlur);
 function onBlur(a) {
     const inp = a.currentTarget;
     const inpLength = Number(inp.dataset.length);
 
     if (inp.value.length === inpLength) {
-        inp.classList.remove('invalid');
-        inp.classList.add('valid');
+        inp.classList.remove("invalid");
+        inp.classList.add("valid");
     }
     else {
-        inp.classList.remove('valid');
-        inp.classList.add('invalsd');
+        inp.classList.remove("valid");
+        inp.classList.add("invalid");
     }
  }
+
--- js/task-08.js
+++ js/task-08.js
@@ -1,15 +1,56 @@
-const form = document.querySelector('.login-form');
-form.addEventListener('submit', onFormSubmit);
-function onFormSubmit(event) {
-    event.preventDefault();
-    const formElements = event.currentTarget.elements;
-    console.dir(formElements);
+const checkForm = document.querySelector(".login-form");
+console.log(checkForm);
 
-    const email = formElements.email.value;
-    const password = formElements.password.value;
-    if (email.value === "" || password.value === "") {
-         return albert('Заполни форму!')
-    }
-    console.log({ email: email.value, password: password.value });
+checkForm.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+ const {
+    elements: { email, password }
+ } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+   return alert("All credentials fill you must...");
+  }
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
 }
 
+ 
+
+// ***
+  // const { elements } = event.currentTarget;
+  // const { email, password } = elements;
+//  ***
+  // const { email, password } = event.target.elements;
+
+// вариант 2
+
+// const checkForm = document.querySelector(".login-form");
+// console.log(checkForm);
+
+// checkForm.addEventListener("submit", handleSubmit);
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   // const email = event.currentTarget.elements.email.value;
+//   // const pass = event.currentTarget.elements.password.value;
+//   const formData = new FormData(event.currentTarget);
+//   const data = {};
+//   formData.forEach((name, value) => {
+//     if (!name || !value) {
+//       alert("All credentials fill you must...");
+//     } else {
+//       data[value] = name;
+//     }
+//   });
+//   if (data.email && data.password) {
+//     console.log(data);
+//     event.currentTarget.reset();
+//   }
+// }
+
+  
+ 
+  // const isValidForm = [...event.target.elements].filter((el) => el.tagName === "INPUT").some((el) => el.value === "");
+
+  // if (isValidForm) {
+  //   alert("All credentials fill you must...")
+  // 
